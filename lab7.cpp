2ac8b8b9f07efa48c4329f217865fe6c05d77642
Alina <lavrinenkoalinav@gmail.com>
2024-10-31 15:42:29 +0300

    lab7.cpp

---
+++ lab7.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int arr[] = { 37, 47, 92, 10, 28, 85 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    k = k % n;
+
+    cout << "Початковий масив: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int* temp = new int[k];
+    for (int i = 0; i < k; ++i) {
+        temp[i] = arr[i];
+    }
+
+    for (int i = 0; i < n - k; ++i) {
+        arr[i] = arr[i + k];
+    }
+
+    for (int i = 0; i < k; ++i) {
+        arr[n - k + i] = temp[i];
+    }
+
+    delete[] temp;
+
+    cout << "Массив после циклического сдвига: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
